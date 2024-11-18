6fbc687002f59bbe165e207f9a7582d357590bd3
Adiela Abishua <adiela.abishua@gmail.com>
2024-11-18 21:30:55 +0300

    Create serializers, views, and routes for djangorestframework auth

--- backend/myauth/views.py
+++ backend/myauth/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import serializers, viewsets
 
-# Create your views here.
+User = get_user_model()
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
