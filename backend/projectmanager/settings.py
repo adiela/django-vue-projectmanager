de928b3277531ff0adb4b66688b9577e4d16b231
Adiela Abishua <adiela.abishua@gmail.com>
2024-11-19 19:31:37 +0300

    Get secret ettings from env

--- backend/projectmanager/settings.py
+++ backend/projectmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@otvwxh07#@+-h2%qg58=kkvw7x2xh1t=830c-+kp@@f!+lzfo'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if config('ENVIRONMENT') == 'development':
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+if config('ENVIRONMENT') == 'development':
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+else:
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
