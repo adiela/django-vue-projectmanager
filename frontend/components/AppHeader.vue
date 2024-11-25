4e5cd41c190f101db8e4c7610156f582aacebe0b
Adiela Abishua <adiela.abishua@gmail.com>
2024-11-25 09:36:38 +0300

    AppHeader add home page link

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -3,7 +3,7 @@
     <nav class="w-full px-8 text-gray-700 bg-white">
         <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
             <div class="relative flex flex-col md:flex-row">
-                <NuxtLink to="#_" class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
+                <NuxtLink to="/" class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
                     <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">Project Manager<span class="text-teal-600">.</span></span>
                 </NuxtLink>
             </div>
