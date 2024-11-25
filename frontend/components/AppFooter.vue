1c53467c87a3c5c9c70879373d343a084c70ff85
Adiela Abishua <adiela.abishua@gmail.com>
2024-11-25 09:23:19 +0300

    Edit Footer styling

--- frontend/components/AppFooter.vue
+++ frontend/components/AppFooter.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- Footer -->
     <footer class="bg-white">
-        <div class="max-w-screen-xl px-4 py-12 mx-auto overflow-hidden sm:px-6 lg:px-8">
-            <p class="mt-8 text-base leading-6 text-center text-gray-400">
+        <div class="max-w-screen-xl px-4 py-8 mx-auto overflow-hidden sm:px-6 lg:px-8">
+            <p class="text-base leading-6 text-center text-gray-400">
                 © {{ new Date().getFullYear() }} Built by <a href="https://gtihub.com/adiela" class="text-teal-600">Adiela Abishua</a>. Licensed under the MIT License.
             </p>
         </div>
